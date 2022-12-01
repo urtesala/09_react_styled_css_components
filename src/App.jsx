099b5cfc93059f424032d5537e6be2692dc29394
urtesala <urtesaladziute@gmail.com>
2022-12-01 09:23:51 +0300

    styled components, button

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,36 @@
-import './App.css'
+import styled from 'styled-components';
+import './App.css';
+
+// sukuriam h1 elementa pavadinimu title naudodami styled components
+const Title = styled.h1`
+  font-size: 3rem;
+  font-weight: normal;
+  text-decoration: underline;
+`;
+
+//sukurti button komponenta, prideti jam stiliu ir panaudoti jsx
+
+const Btn = styled.button`
+  font-size: 2rem;
+  padding: 1em 2em;
+  border-radius: 20px;
+  border: none;
+  background-color: #ba5454;
+  color: white;
+  cursor: pointer;
+  transition: all 0.2s ease-out;
+  &:hover {
+    background-color: #573535;
+  }
+`;
 
 function App() {
   return (
-    <div className="App container">
-      <h1>React styled components</h1>
+    <div className='App container'>
+      <Title>React styled components</Title>
+      <Btn>Mygtukas</Btn>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
